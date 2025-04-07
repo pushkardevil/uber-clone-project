60b95c40c27259893b45553eeee67d8de7a48f57
Pushkar Kumar <20bsm044@iiitdmj.ac.in>
2025-04-07 13:28:34 +0530

    adding files

--- Backend/app.js
+++ Backend/app.js
@@ -4,19 +4,19 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const cookieParser = require('cookie-parser');
-// const connectToDb = require('./db/db');
-// const userRoutes = require('./routes/user.routes');
-// const captainRoutes = require('./routes/captain.routes');
-// const mapsRoutes = require('./routes/maps.routes');
-// const rideRoutes = require('./routes/ride.routes');
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
-// connectToDb();
+connectToDb();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
@@ -24,10 +24,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// app.use('/users', userRoutes);
-// app.use('/captains', captainRoutes);
-// app.use('/maps', mapsRoutes);
-// app.use('/rides', rideRoutes);
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 
